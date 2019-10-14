e2d44e2233d4ffcc5c89bceb6dc356729b279bdd
ncastle16 <ncastle16@wou.edu>
2019-10-14 12:32:20 -0700

    CSS file added. JS changed and working.

--- HW2/CS460HW2.js
+++ HW2/CS460HW2.js
@@ -1,5 +1,3 @@
-const dollarType = ["USD", "Euro", "Yuan", "Peso"];
-
 function tipTable(amount, tip, split){
     "use strict";
     
@@ -7,8 +5,18 @@ function tipTable(amount, tip, split){
         "class": "tipTable" 
     });
 
-    var tcaption = $("<caption>").html("$" + amount);
-    tcaption.appendTo(tabl);
+
+    if(split == ''){
+        split = 1;
+    }
+
+    if(amount == '') {
+        return "Fill in the provided boxes, it is not that hard!";
+    }
+
+    else if(split > 1){
+    var title = $("<p>").html("$" + amount + " check with " + tip + "% tip split amongst " + split + " people");
+    title.appendTo("#theNums");
 
     var thead = $("<thead>\
 			      <tr>\
@@ -25,40 +33,97 @@ function tipTable(amount, tip, split){
     var trow = $("<tr>");
     var n = null;
     n = amount * (tip / 100) / split;
+    n = n.toFixed(2);
+
     tdat = $("<td>", {
         id: n
     }).html(n);
     tdat.appendTo(trow);
     
     n = (1.1 * amount) * (tip / 100) / split;
+    n = n.toFixed(2);
     tdat = $("<td>", {
         id: n
     }).html(n);
     tdat.appendTo(trow);
 
     n = (0.14 * amount) * (tip / 100) / split;
+    n = n.toFixed(2);
     tdat = $("<td>", {
         id: n
     }).html(n);
     tdat.appendTo(trow);
 
     n = ( .052 * amount) * (tip / 100) / split;
+    n = n.toFixed(2);
     tdat = $("<td>", {
         id: n
     }).html(n);
     tdat.appendTo(trow);
 
-    trow.appendTo(tabl);
-    
+    trow.appendTo(tbody);
+    tbody.appendTo(tabl);
+
     return tabl;
 }
 
-$("#gen").submit(function (event) {
+else{
+var title = $("<p>").html("$" + amount + " check with " + tip + "% tip");
+    title.appendTo("#theNums");
+
+    var thead = $("<thead>\
+			      <tr>\
+                     <th>USD</th>\
+                     <th>Euro</th>\
+                     <th>Yuan</th>\
+                     <th>Peso</th>\
+			      </tr>\
+				</thead>");
+    thead.appendTo(tabl);
+    var tbody = $("<tbody>")
+    var tdat = $("<td>");
+    var trow = $("<tr>");
+    var n = null;
+    n = amount * (tip / 100) / split;
+    n = n.toFixed(2);
+    tdat = $("<td>", {
+        id: n
+    }).html(n);
+    tdat.appendTo(trow);
+    
+    n = (1.1 * amount) * (tip / 100) / split;
+    n = n.toFixed(2);
+    tdat = $("<td>", {
+        id: n
+    }).html(n);
+    tdat.appendTo(trow);
+
+    n = (0.14 * amount) * (tip / 100) / split;
+    n = n.toFixed(2);
+    tdat = $("<td>", {
+        id: n
+    }).html(n);
+    tdat.appendTo(trow);
+
+    n = ( .052 * amount) * (tip / 100) / split;
+    n = n.toFixed(2);
+    tdat = $("<td>", {
+        id: n
+    }).html(n);
+    tdat.appendTo(trow);
+
+    trow.appendTo(tbody);
+    tbody.appendTo(tabl);
+    
+    return tabl;
+}
+}
+
+$("#gen").submit(function(event) {
     event.preventDefault();
     var amount = $("#id1").val();
     var tip = $("#id2").val();
     var split = $("#id3").val();
-    console.log(amount, tip, split);
         $("#theNums").empty();
         $("#theNums").append(tipTable(amount, tip, split));
         $("#dataEntry").html("");
