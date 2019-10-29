784c8bc2b8bee4b54bde446463c4fd7fd504678a
ncastle16 <ncastle16@wou.edu>
2019-10-28 17:52:51 -0700

    First push, part 1 working.

---
+++ HW4/CS460HW4/Controllers/InterporlatorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CS460HW4.Controllers
+{
+    public class InterporlatorController : Controller
+    {
+        // GET: Interporlator
+        public ActionResult Interporlator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Interporlator(string FirstColor, string SecondColor, int? Amount)
+        {
+            ViewBag.FC = FirstColor;
+            ViewBag.SC = SecondColor;
+            ViewBag.Amount = Amount;
+
+            return View();
+        }
+    }
+}
---
+++ HW4/CS460HW4/Controllers/RGBController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CS460HW4.Controllers
+{
+    public class RGBController : Controller
+    {
+        // GET: RGB
+        public ActionResult RGB()
+        {
+            string red = Request["Red"];
+            string green = Request["Green"];
+            string blue = Request["Blue"];
+            ViewBag.red = red;
+            ViewBag.green = green;
+            ViewBag.blue = blue;
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult RGB(int Red, int Green, int Blue)
+        {
+            return View();
+        }
+    }
+}
