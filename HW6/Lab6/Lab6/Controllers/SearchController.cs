11077d82bb1d0078cdfa68b754f388c27193d9ee
ncastle16 <ncastle16@wou.edu>
2019-11-11 23:06:46 -0800

    First push. need to finish making button on search page

---
+++ HW6/Lab6/Lab6/Controllers/SearchController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab6.Models;
+using Lab6.DAL;
+using Lab6.Models.ViewModels;
+
+namespace Lab6.Controllers
+{
+    public class SearchController : Controller
+    {
+        private WorldWideImportersContext db = new WorldWideImportersContext();
+
+        // GET: Search
+        public ActionResult SearchPage(string searchName)
+        {
+            var items = from db in db.StockItems select db;
+
+            if (!String.IsNullOrEmpty(searchName))
+            {
+                items = items.Where(model => model.StockItemName.Contains(searchName));
+            }
+            // Current projects
+            return View(db.StockItems.Where(model => model.StockItemName.Contains(searchName)));
+            
+        }
+
+
+        public ActionResult Details(int ID)
+        {
+            StockItem stockItem = db.StockItems.Find(ID);
+            ProductDetailViewModel viewModel = new ProductDetailViewModel(stockItem);
+            return View(viewModel);
+        }
+    }
+}
---
+++ HW6/Lab6/Lab6/Models/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab6.Models.ViewModels
+{
+    public class ProductDetailViewModel
+    {
+        public ProductDetailViewModel(StockItem stockItem)
+        {
+            ItemName = stockItem.StockItemName;
+            UnitPrice = stockItem.UnitPrice;
+            TypicalWeightPerUnit = stockItem.TypicalWeightPerUnit;
+            LeadTimeDays = stockItem.LeadTimeDays;
+            ValidFrom = stockItem.ValidFrom;
+            CustomFields = stockItem.CustomFields;
+            Tags = stockItem.Tags;
+            Size = stockItem.Size;
+            Company = stockItem.Brand;
+        }
+        public string ItemName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TypicalWeightPerUnit { get; set; }
+        public int LeadTimeDays { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public string CustomFields { get; set; }
+        public string Tags { get; set; }
+
+        public string Size { get; set; }
+
+
+        public string Company { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Website { get; set; }
+        public string Contact { get; set; }
+
+    }
+}
