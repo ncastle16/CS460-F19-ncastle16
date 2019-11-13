aca679fbf1ff67bb3bfad68d23e5a3704a6cbb30
ncastle16 <ncastle16@wou.edu>
2019-11-13 13:37:13 -0800

    just need to get top purchasers working

--- HW6/Lab6/Lab6/Models/ViewModels/ProductDetailViewModel.cs
+++ HW6/Lab6/Lab6/Models/ViewModels/ProductDetailViewModel.cs
@@ -9,16 +9,24 @@ namespace Lab6.Models.ViewModels
     {
         public ProductDetailViewModel(StockItem stockItem)
         {
+
             ItemName = stockItem.StockItemName;
             UnitPrice = stockItem.UnitPrice;
             TypicalWeightPerUnit = stockItem.TypicalWeightPerUnit;
             LeadTimeDays = stockItem.LeadTimeDays;
             ValidFrom = stockItem.ValidFrom;
-            CustomFields = stockItem.CustomFields;
+            CustomFields = stockItem.CustomFields.Split('"')[3]; ;
             Tags = stockItem.Tags;
             Size = stockItem.Size;
-            Company = stockItem.Brand;
-        }
+            Company = stockItem.Supplier.SupplierName;
+            Phone = stockItem.Supplier.PhoneNumber;
+            Fax = stockItem.Supplier.FaxNumber;
+            Website = stockItem.Supplier.WebsiteURL;
+            Orders = stockItem.OrderLines.Where(i => i.StockItemID == stockItem.StockItemID).Count();
+            GrossSales = stockItem.InvoiceLines.Where(i => i.StockItemID == stockItem.StockItemID).Sum(i => i.ExtendedPrice);
+            GrossProfit = stockItem.InvoiceLines.Where(i => i.StockItemID == stockItem.StockItemID).Sum(i => i.LineProfit);
+            TopPurchasers = stockItem.InvoiceLines.Where().db.Invoice.Customer.CustomerName;
+
         public string ItemName { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal TypicalWeightPerUnit { get; set; }
@@ -29,12 +37,19 @@ namespace Lab6.Models.ViewModels
 
         public string Size { get; set; }
 
-
+        //Supplier Profile
         public string Company { get; set; }
         public string Phone { get; set; }
         public string Fax { get; set; }
         public string Website { get; set; }
         public string Contact { get; set; }
 
+        //Sales History Summary
+        public int Orders { get; set; }
+        public decimal GrossSales { get; set; }
+        public decimal GrossProfit { get; set; }
+
+        //Top Purchasers
+        public List<string> TopPurchasers { get; set; }
     }
 }
